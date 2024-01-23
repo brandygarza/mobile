7def8e906bd52ccc6de5daef775229065836c32a
Student <student@Mac.local>
2024-01-23 09:51:24 -0800

    Add more stuff

---
+++ HelloWorld/HelloWorld/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace HelloWorld;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		if(lblDisplay.Text == "Hello World")
+		{
+			lblDisplay.Text = "Good Day";
+		}
+		else
+		{
+			lblDisplay.Text = "Hello World";
+		}
+	}
+}
+
+
